40e3187175e66a8837fc17c26da102e2371854ec
fw <fwe>
2025-08-24 12:08:27 +0800

    Initial Commit

---
+++ ff/ffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ffApp.swift
+//  ff
+//
+//  Created by OH KAIXIU on 24/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
